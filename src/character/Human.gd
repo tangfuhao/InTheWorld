06344c6182ee22d3cc208e3f68dcc428ea7e7626
付豪 <fangshiyu2@gmail.com>
2020-08-03 18:23:22 +0800

    第一次提交 基本场景 角色 AI  巡逻

---
+++ src/character/Human.gd
@@ -0,0 +1,76 @@
+extends KinematicBody2D
+
+
+export var speed_max := 300
+export var acceleration_max := 300
+export var rotation_speed_max := 360
+export var rotation_accel_max := 2000
+
+
+#运动属性
+var velocity := Vector2.ZERO
+var angular_velocity := 0.0
+var direction := Vector2.UP
+
+
+#ai steering
+onready var agent := GSAISteeringAgent.new()
+onready var proxy_target := GSAIAgentLocation.new()
+onready var face := GSAIFace.new(agent, proxy_target)
+onready var accel := GSAITargetAcceleration.new()
+
+#巡逻
+onready var wanderController = $WanderController
+onready var wanderController_Position = $WanderController/Position2D
+
+func _ready() -> void:
+	agent.linear_speed_max = speed_max
+	agent.linear_acceleration_max = acceleration_max
+	agent.angular_speed_max = deg2rad(rotation_speed_max)
+	agent.angular_acceleration_max = deg2rad(rotation_accel_max)
+	agent.bounding_radius = 10
+	update_agent()
+	update_wander()
+
+
+	face.alignment_tolerance = deg2rad(5)
+	face.deceleration_radius = deg2rad(45)
+	
+	
+func update_wander():
+	wanderController.rotation_degrees = rand_range(0,360)
+	#print(wanderCircle.rotation_degrees)
+	
+	proxy_target.position.x = wanderController_Position.global_position.x
+	proxy_target.position.y = wanderController_Position.global_position.y
+	
+
+func _physics_process(delta: float) -> void:
+	update_agent()
+	update_wander()
+
+	var movement := -1
+	direction = GSAIUtils.angle_to_vector2(rotation).normalized()
+	velocity = velocity.move_toward(direction * speed_max * movement,delta * acceleration_max);
+	velocity = move_and_slide(velocity)
+	
+	
+
+	face.calculate_steering(accel)
+	if accel.angular == 0:
+		angular_velocity = 0
+	else:
+		angular_velocity += accel.angular * delta
+		angular_velocity = clamp(angular_velocity, -agent.angular_speed_max, agent.angular_speed_max)
+		rotation += angular_velocity * delta
+	
+
+
+func update_agent() -> void:
+	agent.position.x = global_position.x
+	agent.position.y = global_position.y
+	agent.orientation = rotation
+	agent.linear_velocity.x = velocity.x
+	agent.linear_velocity.y = velocity.y
+	agent.angular_velocity = angular_velocity
+
---
+++ src/character/LabelLayout.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _process(_delta):
+	global_rotation_degrees = 0
