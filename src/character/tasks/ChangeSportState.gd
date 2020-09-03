2741349c34be241be74bdc77cd4f85cf666abab7
付豪 <fangshiyu2@gmail.com>
2020-09-03 20:03:11 +0800

    先暂停 运动方式更改

--- src/character/tasks/ChangeSportState.gd
+++ src/character/tasks/ChangeSportState.gd
@@ -4,7 +4,7 @@ class_name ChangeSportState
 func active() ->void:
 	.active()
 	if human:
-		human.set_status_value("体力状态",0.3)
+		#human.set_status_value("体力状态",0.3)
 		goal_status = STATE.GOAL_COMPLETED
 	else:
 		goal_status = STATE.GOAL_FAILED
