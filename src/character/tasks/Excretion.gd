d68b6547c4c9bd238aab1688de4a9c4cb8f33bfb
付豪 <fangshiyu2@gmail.com>
2020-09-03 20:17:03 +0800

    动机修改

--- src/character/tasks/Excretion.gd
+++ src/character/tasks/Excretion.gd
@@ -7,8 +7,10 @@ func active():
 		if target and human.is_approach(target):
 			human.global_position.x = target.global_position.x
 			human.global_position.y = target.global_position.y
+			human.set_status_value("排泄状态",1)
 			print(human.player_name,"在",target.stuff_name,"排泄")
+			goal_status = STATE.GOAL_COMPLETED
 		else:
-			print(human.player_name,"在排泄了")
+			print(human.player_name,"直接排泄了")
 	else:
 		goal_status = STATE.GOAL_FAILED
