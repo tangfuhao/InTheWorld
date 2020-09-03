dede45cca47a63367c6e6f7b5db39acbdebe820a
付豪 <fangshiyu2@gmail.com>
2020-09-03 20:01:52 +0800

    接入函数计算

--- src/character/strategy/StrategyItemModel.gd
+++ src/character/strategy/StrategyItemModel.gd
@@ -1,5 +1,5 @@
 class_name StrategyItemModel
-var weight := 1
+var weight :float= 1
 var pre_condition_arr:Array
 var adjust_task_finish_condition:Array
 var task_queue:Array
@@ -15,8 +15,20 @@ func calculate_weight(_strategy_weight_variable_dic:Dictionary):
 	if weight_calculate_arr and not weight_calculate_arr.empty():
 		var weight_param_value_name = weight_calculate_arr[0]
 		var weight_param_value = _strategy_weight_variable_dic[weight_param_value_name]
-		weight = evaluateResult(weight_param_value,weight_calculate_arr[1],float(weight_calculate_arr[2]))
-	
+		if weight_calculate_arr.size() == 3:
+			weight = evaluateResult(weight_param_value,weight_calculate_arr[1],float(weight_calculate_arr[2]))
+		else:
+			weight = evaluateFormula(_strategy_weight_variable_dic,weight_calculate_arr[1])
+			print("复合函数 权重计算:",weight)
+
+func evaluateFormula(_strategy_weight_variable_dic, expression) -> float:
+	var parser = FormulaParser.new()
+	var result = parser.parse(expression, {}, _strategy_weight_variable_dic)
+	if result <= 0:
+		result = 0.01
+	if result > 1:
+		result = 1
+	return result
 
 
 func evaluateResult(property, condition, value) -> float:
