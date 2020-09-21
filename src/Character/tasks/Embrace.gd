5904877bb03480b4f85f15a2a657c85905ecbce5
付豪 <fangshiyu2@gmail.com>
2020-09-21 22:29:13 +0800

    行为  恢复  状态值

--- src/Character/tasks/Embrace.gd
+++ src/Character/tasks/Embrace.gd
@@ -15,4 +15,12 @@ func active():
 		goal_status = STATE.GOAL_FAILED
 		return
 
+	action_target.interaction_action(human,action_name)
+	status_recover = true
 	goal_status = STATE.GOAL_COMPLETED
+
+
+func terminate() ->void:
+	.terminate()
+	if status_recover:
+		human.set_status_value("爱情状态",0.9)
