57f8e6d9c3c60c84dce31027d36b1de4828dae29
付豪 <fangshiyu2@gmail.com>
2020-10-09 11:30:49 +0800

    自定义物品的面板 参数微调

--- src/UI/customization/listview/ListViewItemType2.gd
+++ src/UI/customization/listview/ListViewItemType2.gd
@@ -1,20 +1,16 @@
 extends Panel
-onready var label = $HBoxContainer/Panel/Label
-onready var dropdown = $HBoxContainer/Control/OptionButton
-onready var text_edit = $HBoxContainer/Control/TextEdit
+onready var label = $HBoxContainer/Label
+onready var dropdown = $HBoxContainer/OptionButton
+onready var text_edit = $HBoxContainer/TextEdit
 
-var is_show_selector = false
+
 var item_index
 signal item_value_change(index,key,value)
 
 
 	
 func set_selector_arr(selector_arr:Array,_selected_value):
-	if selector_arr && not selector_arr.empty(): 
-		is_show_selector = true
-		dropdown.visible = true
-		text_edit.visible = false
-		
+	if selector_arr and not selector_arr.empty(): 
 		var index = 0
 		var default_index = 0
 		for item in selector_arr:
@@ -23,27 +19,29 @@ func set_selector_arr(selector_arr:Array,_selected_value):
 				default_index = index
 			index = index + 1 
 		dropdown.select(default_index)
+		set_edit_value(_selected_value)
 	else:
-		is_show_selector = false
 		dropdown.visible = false
-		text_edit.visible = true
+
 	
 func set_label(_display_label):
 	label.text = _display_label
 
 func set_edit_value(value):
 	if value:
-		text_edit.text = value
+		text_edit.text = String(value)
 	
 func get_key_and_value():
 	var label_str = label.text
-	if is_show_selector:
-		var id = dropdown.get_selected_id()
-		var select_item_text = dropdown.get_item_text(id)
-		return [label_str,select_item_text]
-	else:
-		var edit_text = text_edit.text
-		return [label_str,edit_text]
+	var edit_text = text_edit.text
+	return [label_str,edit_text]
+#	if is_show_selector:
+#		var id = dropdown.get_selected_id()
+#		var select_item_text = dropdown.get_item_text(id)
+#		return [label_str,select_item_text]
+#	else:
+#		var edit_text = text_edit.text
+#		return [label_str,edit_text]
 
 
 
@@ -57,4 +55,5 @@ func _on_OptionButton_item_selected(index):
 	var key = label.text
 	var id = dropdown.get_selected_id()
 	var value = dropdown.get_item_text(id)
+	set_edit_value(value)
 	emit_signal("item_value_change",item_index,key,value)
