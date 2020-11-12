5f16939bf4f1959dd3f7a6537f5aefe97286ed31
付豪 <fangshiyu2@gmail.com>
2020-11-12 17:06:44 +0800

    属性影响的一种 优化方式

--- src/World/Interaction/NodeEffect/NodeParamEffect.gd
+++ src/World/Interaction/NodeEffect/NodeParamEffect.gd
@@ -3,18 +3,28 @@ class_name NodeParamEffect
 var node_name
 var param_name
 var transform setget set_transform
-var assign
+var assign setget set_assign
 
 var temp_delta = 0
+#判断是否是计算公式
+var is_expression = false
 
-
-
+func check_is_expression(_expression):
+	var sign_arr := ["+","-","*","/","${"]
+	for item in sign_arr:
+		if item in _expression:
+			return true
+	return false
 
 func set_transform(_transform):
+	transform = _transform
 	if _transform is String:
-		transform = _transform
-	else:
-		transform = _transform
+		is_expression = check_is_expression(transform)
+
+func set_assign(_assign):
+	assign = _assign
+	if _assign is String:
+		is_expression = check_is_expression(_assign)
 
 
 func _process(_delta,_param_accessor):
@@ -25,16 +35,25 @@ func _process(_delta,_param_accessor):
 		
 	if transform != null:
 		var value = node.get_param_value(param_name)
-		var t = 0
-		if transform is String:
+		var value_change
+		if is_expression:
 			var parser = FormulaParser.new("\\$\\{(.+?)\\}")
-			t = parser.parse(transform, {}, {},_param_accessor) * temp_delta
+			value_change = parser.parse(transform, {}, {},_param_accessor)
+			value_change = value_change * temp_delta
+			value = value + value_change
 		else:
-			t = transform * temp_delta
-	
-		value = value + t
+			if transform is String:
+				value = value_change
+			else:
+				value_change = transform * temp_delta
 		node.set_param_value(param_name,value)
 	elif assign != null:
-		node.set_param_value(param_name,assign)
+		if is_expression:
+			var parser = FormulaParser.new("\\$\\{(.+?)\\}")
+			var assign_value = parser.parse(assign, {}, {},_param_accessor)
+			node.set_param_value(param_name,assign_value)
+		else:
+			node.set_param_value(param_name,assign)
+		
 	
 	temp_delta = 0
