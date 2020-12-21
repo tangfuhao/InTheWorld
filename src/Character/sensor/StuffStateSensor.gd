a87dc98069ea2f52217ee88c7cceef574be24d38
付豪 <fangshiyu2@gmail.com>
2020-12-21 15:02:19 +0800

    周围情况的感知

--- src/Character/sensor/StuffStateSensor.gd
+++ src/Character/sensor/StuffStateSensor.gd
@@ -2,17 +2,13 @@ class_name StuffStateSensor
 extends Sensor
 #物品状态感知
 
-#激活周围有物品的属性
-func active_aroud_stuff_type(_type_name):
-	var node_type_group = DataManager.get_node_parent_type_group(_type_name)
-	for item in node_type_group:
-		world_status.set_world_status("周围有-%s"%item,true)
+
 
 func setup(_control_node,_world_status):
 	.setup(_control_node,_world_status)
-	var vision_sensor = control_node.vision_sensor
+	var vision_sensor:VisionSensor = control_node.vision_sensor
 	vision_sensor.connect("vision_find_stuff",self,"on_vision_find_stuff")
-	vision_sensor.connect("vision_find_stuff",self,"on_vision_lost_stuff")
+	vision_sensor.connect("vision_lost_stuff",self,"on_vision_lost_stuff")
 	
 	var monitoring_arr_type_dic = vision_sensor.monitoring_arr_type_dic
 	for node_type_item in monitoring_arr_type_dic.keys():
@@ -22,18 +18,21 @@ func on_vision_find_stuff(_body):
 	var type_name = _body.stuff_type_name
 	active_aroud_stuff_type(type_name)
 
-
 func on_vision_lost_stuff(_body):
-	pass
-#	var type = _body.stuff_type_name
-#	world_status.set_world_status("周围有-"%type,false)
+	var type_name = _body.stuff_type_name
+	var vision_sensor:VisionSensor = control_node.vision_sensor
+	var type_node_arr = vision_sensor.get_monitoring_arr_by_type(type_name)
+	if not type_node_arr.empty():
+		return
 	
-	
-#func _on_stuff_broke_change(_stuff):
-#	if _stuff.stuff_name == "淋浴间":
-#		world_status.set_world_status("淋浴间可用",not _stuff.is_broke)
-#
-#func _on_stuff_state_change(_stuff):
-#	if _stuff.stuff_name == "淋浴间":
-#		var be_occupy = _stuff.is_occupy() and not _stuff.is_occupy_player(control_node)
-#		world_status.set_world_status("淋浴间被占用",be_occupy)
+	var node_type_group = DataManager.get_node_parent_type_group(type_name)
+	for node_type_item in node_type_group:
+		type_node_arr = vision_sensor.get_monitoring_arr_by_type(node_type_item)
+		if type_node_arr.empty():
+			world_status.set_world_status("周围有-%s"%node_type_item,false)
+
+#激活周围有物品的属性
+func active_aroud_stuff_type(_type_name):
+	var node_type_group = DataManager.get_node_parent_type_group(_type_name)
+	for item in node_type_group:
+		world_status.set_world_status("周围有-%s"%item,true)
