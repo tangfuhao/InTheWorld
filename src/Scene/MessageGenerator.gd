c25ec1094fe6ea19ad62d8c707a656949063a16d
付豪 <fangshiyu2@gmail.com>
2021-02-19 14:36:30 +0800

    消息生成  路径更新

--- src/Scene/MessageGenerator.gd
+++ src/Scene/MessageGenerator.gd
@@ -1,7 +1,8 @@
 #全局消息产生器
 class_name MessageGenerator
 
-signal message_dispatch
+signal message_dispatch(_message_dic)
+signal message_dispatch2(_player_node,_message_dic)
 
 enum MessageType { execute_action, stop_action, find_player_in_vision,lost_player_in_vision,find_in_vision,lost_in_vision,location }
 
@@ -14,8 +15,9 @@ func create_common_message(_player,_type):
 	message_dic["type"] = _type
 	return message_dic
 	
-func send_message_to_subscriber(_message_dic):
+func send_message_to_subscriber(_player,_message_dic):
 	emit_signal("message_dispatch",_message_dic)
+	emit_signal("message_dispatch2",_player,_message_dic)
 
 #角色执行动作
 func send_player_action(_player,_action,_target):
@@ -29,7 +31,7 @@ func send_player_action(_player,_action,_target):
 		message_dic["target"] = _target.node_name
 		message_dic["target_display_name"] = _target.display_name
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 	
 
 #角色停止执行行为
@@ -44,7 +46,7 @@ func send_player_stop_action(_player,_action,_target):
 		message_dic["target"] = _target.node_name
 		message_dic["target_display_name"] = _target.display_name
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 #角色视线检测
 func send_player_find_player_in_vision(_player,_target):
@@ -53,7 +55,7 @@ func send_player_find_player_in_vision(_player,_target):
 	var message_dic = create_common_message(_player,"find_player_in_vision")
 	message_dic["target"] = _target.node_name
 	message_dic["target_display_name"] = _target.display_name
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 	
 func send_player_lost_player_in_vision(_player,_target):
 	if not _player: return 
@@ -61,7 +63,7 @@ func send_player_lost_player_in_vision(_player,_target):
 	var message_dic = create_common_message(_player,"lost_player_in_vision")
 	message_dic["target"] = _target.node_name
 	message_dic["target_display_name"] = _target.display_name
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 	
 func send_player_find_stuff_in_vision(_player,_target):
 	if not _player: return 
@@ -69,7 +71,7 @@ func send_player_find_stuff_in_vision(_player,_target):
 	var message_dic = create_common_message(_player,"find_in_vision")
 	message_dic["target"] = _target.node_name
 	message_dic["target_display_name"] = _target.display_name
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 	
 func send_player_lost_stuff_in_vision(_player,_target):
 	if not _player: return 
@@ -77,7 +79,7 @@ func send_player_lost_stuff_in_vision(_player,_target):
 	var message_dic = create_common_message(_player,"lost_in_vision")
 	message_dic["target"] = _target.node_name
 	message_dic["target_display_name"] = _target.display_name
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 #角色位置变化
 func send_player_location_change(_player,_location):
@@ -85,7 +87,7 @@ func send_player_location_change(_player,_location):
 	
 	var message_dic = create_common_message(_player,"location")
 	message_dic["target"] = _location
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 	
 #角色认知更改认知
 func send_player_world_status_change(_player,_world_status,_value):
@@ -95,7 +97,7 @@ func send_player_world_status_change(_player,_world_status,_value):
 	message_dic["target"] = _world_status
 	message_dic["value"] = _value
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 #最高优先级动机值变更
 func send_highest_priority_motivation(_player,_motivation):
@@ -105,7 +107,7 @@ func send_highest_priority_motivation(_player,_motivation):
 	message_dic["target"] = _motivation.motivation_name
 	message_dic["value"] = _motivation.motivation_value
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 #角色动机值改变	
 func send_player_motivation_value_change(_player,_motivation):
@@ -114,27 +116,27 @@ func send_player_motivation_value_change(_player,_motivation):
 	message_dic["target"] = _motivation.motivation_name
 	message_dic["value"] = _motivation.motivation_value
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 #角色目标切换
 func send_player_target_change(_player,_target):
 	var message_dic = create_common_message(_player,"target_change")
 	message_dic["target"] = _target.node_name
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 func send_player_strategy_plan_succuss(_player,_strategy_chain):
 	var strategy_record_arr =  _strategy_chain.strategy_record_arr
 	for item in strategy_record_arr:
 		var message_dic = create_common_message(_player,"strategy_plan_succuss")
 		message_dic["target"] = item
-		send_message_to_subscriber(message_dic)
+		send_message_to_subscriber(_player,message_dic)
 
 func send_player_strategy_plan_fail(_player,_strategy_chain):
 	var strategy_record_arr =  _strategy_chain.strategy_record_arr
 	for item in strategy_record_arr:
 		var message_dic = create_common_message(_player,"strategy_plan_fail")
 		message_dic["target"] = item
-		send_message_to_subscriber(message_dic)
+		send_message_to_subscriber(_player,message_dic)
 
 
 
@@ -145,7 +147,7 @@ func send_player_strategy_plan(_player,_strategy_plan_path,_plan_result):
 	message_dic["target"] = _strategy_plan_path
 	message_dic["value"] = _plan_result
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 #喜爱值改变
 func send_player_lover_value_change(_player,_target,_lover_value):
@@ -154,7 +156,7 @@ func send_player_lover_value_change(_player,_target,_lover_value):
 	message_dic["target_display_name"] = _target.display_name
 	message_dic["value"] = _lover_value
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
 
 func send_player_lover_effect(_player,_target,_effect_value,_action):
 	var message_dic
@@ -167,4 +169,4 @@ func send_player_lover_effect(_player,_target,_effect_value,_action):
 	message_dic["target_display_name"] = _target.display_name
 	message_dic["value"] = _action
 	
-	send_message_to_subscriber(message_dic)
+	send_message_to_subscriber(_player,message_dic)
