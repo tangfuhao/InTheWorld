849af4f6b58b8683dc0ce5c7ec29359b9b312e88
付豪 <fangshiyu2@gmail.com>
2021-02-19 14:32:55 +0800

    快速获取游戏场景节点 api

--- src/Global/FunctionTool.gd
+++ src/Global/FunctionTool.gd
@@ -3,6 +3,6 @@ extends Node
 
 #快速获取游戏包装节点
 func get_game_wapper_node(node_path:NodePath) ->Node2D:
-	var game_wapper_node_name = node_path.get_name(2)
-	var game_wapper_ref = get_node("/root/Lobby/"+game_wapper_node_name)
+	var game_wapper_node_name = node_path.get_name(3)
+	var game_wapper_ref = get_node("/root/Lobby/GameNodes/"+game_wapper_node_name)
 	return game_wapper_ref
